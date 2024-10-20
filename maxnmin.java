b0f944075dbb4cbad133a4bbc3ae7a154e599aa8
sampat karehonna <sampatkarehonna@gmail.com>
2024-10-20 17:29:44 +0530

    some change in var name

--- maxnmin.java
+++ maxnmin.java
@@ -6,16 +6,16 @@ public class maxnmin {
         int max = arr[0];
         int min = arr[0];
         int max2 =arr[0];
-        int sencondHighest = 3;
+        int howmuchHighest = 3;
         int n= arr.length;
         for (int i = 0; i < arr.length; i++) {
             max = (arr[i] > max ? arr[i] : max);
             min = (arr[i] < min ? arr[i] : min);
         }
         Arrays.sort(arr);
-        max2=arr[n-sencondHighest];
+        max2=arr[n-howmuchHighest];
         System.out.println("Maximum of the array is " + max);
-        System.out.println( sencondHighest+"nd Maximum of the array is " + max2);
+        System.out.println( howmuchHighest+"nd Maximum of the array is " + max2);
         System.out.println("Minimum of the array is " + min);
     }
 }
